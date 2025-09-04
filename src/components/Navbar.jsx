3880a0232c194c846acc170cb14c7027a3e40c83
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2025-09-04 19:26:47 +0200

    updated navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,25 @@
+import { useGSAP } from "@gsap/react"
 import { navLinks } from "../../constants"
+import gsap from "gsap"
 
 const Navbar = () => {
+
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        start: 'bottom top',
+      }
+    });
+
+    navTween.fromTo('nav', {backgroundColor: 'transparant'}, {
+      backgroundColor: '#00000050',
+      backgroundFilter: 'blur(10px)',
+      duration: 1,
+      ease: 'power1.inOut'
+    });
+  })
+
   return (
     <nav>
         <div>
